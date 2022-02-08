46f4d60889261a1239ae5005bd5cba7a5a9e38cf
M.M.Karmanova@urfu.me <M.M.Karmanova@urfu.me>
2022-02-08 17:41:45 +0500

    Добавьте файлы проекта.

---
+++ RevitAPI_App5_2/Main.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Создайте приложение для изменения типов стен. Приложение должно работать следующим
+//образом: в первую очередь выдаётся запрос для выбора стен. Когда стены выбраны,
+//появляется окно со списком доступных типов стен и кнопкой «Изменить тип». По
+//нажатию на кнопку «Изменить тип», новый тип стен применяется для выбранных
+//стен.
+
+namespace RevitAPI_App5_2
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Сообщение", "Выберите стены, у которых хотите изменить тип. После выбора нажмите Finish");
+
+            var window = new MainView(commandData);
+            window.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPI_App5_2/MainViewViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using RevitAPILibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI_App5_2
+{
+    internal class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SaveCommand { get; }
+        public List<Element> PickedObjects { get; } = new List<Element>();
+        public List<WallType> WallTypes { get; } = new List<WallType>();
+
+        public WallType SelectedWallTypes { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            PickedObjects = SelectionUtils.PickObjects(commandData);
+            WallTypes = RevitAPILibrary.WallUtils.GetWallTypes(commandData);
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (PickedObjects.Count == 0 || SelectedWallTypes == null)
+                return;
+
+            using (var ts = new Transaction(doc, "Set type"))
+            {
+                ts.Start();
+
+                foreach (var pickedObject in PickedObjects)
+                {
+                    if (pickedObject is Wall)
+                    {
+                        var oWall = pickedObject as Wall;
+                        oWall.WallType = SelectedWallTypes;
+                    }
+                }
+                ts.Commit();
+            }
+            RaiseCloseRequest();
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
